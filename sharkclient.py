ed57f13b767b3124f84023a3cbb0c12384bf2a97
jencce zhou <jencce2002@gmail.com>
2013-02-05 15:18:30 +0800

    add command excute

--- sharkclient.py
+++ sharkclient.py
@@ -1,37 +1,51 @@
 #! /usr/bin/env python
 import socket
 import time
+import os
+
+cmdstrings = "sar -n DEV 1 10 > /tmp/sarnDEV-", \
+			 "iostat 1 10 > /tmp/iostat-",	\
+			 "sar -B 1 10 > /tmp/sarB-",	\
+			 "sar -P ALL 1 10 > /tmp/sarPALL-",	\
+			 "vmstat 1 10 > /tmp/vmstat-"
 
 cmdsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cmdsock.bind(('',7778))
 cmdsock.listen(5)
 cs,caddr = cmdsock.accept()
 while True:
-	cmdstr = cs.recv(100)
+	cmdstr = cs.recv(10)
 	if cmdstr[0:6] == 'qwe123':
 		break
 	time.sleep(2)
 
 print cmdstr
+idstring = "lasd"
 
 sersock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sersock.bind(('',7777))
 sersock.listen(5)
 ds,daddr = sersock.accept()
 
-fn = open('/tmp/sar')
-while True:
-	data = fn.read(1024)
-	if not data:
-		break
-	while len(data) > 0:
-		sended = ds.send(data)
-		data = data[sended:]
-
-time.sleep(2)
-ds.send('EOF')
-fn.close()
-
-
-print daddr
+for cs in cmdstrings:
+	ns = cs + idstring
+	print ns
+	ret = os.system(ns)
+	if ret != 0:
+		print ns+"  error"
+		exit()
+	
+	fn = open('/tmp/sar')
+	while True:
+		data = fn.read(1024)
+		if not data:
+			break
+		while len(data) > 0:
+			sended = ds.send(data)
+			data = data[sended:]
+	
+	time.sleep(2)
+	ds.send('EOF')
+	fn.close()
+	
 ds.close()
