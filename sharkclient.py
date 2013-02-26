be1c2c1a8423c2a7402dda228e2c0d1842fac17b
jencce zhou <jencce2002@gmail.com>
2013-02-26 17:30:03 +0800

    3 machine fixup

--- sharkclient.py
+++ sharkclient.py
@@ -20,68 +20,88 @@ cmdstrings = "sar -n DEV 1 10 > /tmp/sarnDEV-", \
 # listen socket to accept cmd connection where get passwd 
 # and requestid.  if connection accepted, end blocking
 #
-listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_sock.bind(('',7778))
-listen_sock.listen(25)
-cmd_sock,caddr = listen_sock.accept()
+def getidstr():
+	os.system('hostname > /tmp/hn')
+	hnfile = open('/tmp/hn')
+	host_name = hnfile.readline()
 
-# handshake to verify passwd
-# cmd socket to get cmd string
-#
-while True:
-	cmdstr = cmd_sock.recv(12)
-	if cmdstr[0:6] == 'qwe123':
-		break
-
-print cmdstr
-
-idstring = cmdstr[cmdstr.find("+")+1:]
-
-cmd_sock.close()
+	listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listen_sock.bind(('',7778))
+	listen_sock.listen(5)
+	cmd_sock,caddr = listen_sock.accept()
+	
+	# handshake to verify passwd # cmd socket to get cmd string
+	#
+	while True:
+		cmdstr = cmd_sock.recv(12)
+		if cmdstr[0:6] == 'qwe123':
+			break
+	
+	cmd_sock.close()
+	listen_sock.close()
+	return cmdstr[cmdstr.find("+")+1:] + host_name
 
 
 # listen socket 2 to accept data connection
 #
-listen_sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_sock2.bind(('',7777))
-listen_sock2.listen(25)
-data_sock,daddr = listen_sock2.accept()
-
-# execute cmds 
-#
-for cmdstring in cmdstrings:
-	cmds = cmdstring + idstring
-	print cmds
-	ret = os.system(cmds)
-	if ret != 0:
-		print cmds+"  error"
-		data_sock.close()
-		exit()
+def sendfiles(idstring):
+	listen_sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listen_sock2.bind(('',7777))
+	listen_sock2.listen(5)
+	data_sock,daddr = listen_sock2.accept()
 	
-	file_name = cmds[cmds.find("/tmp"):]
-	print file_name
-
-	# send magic and filename
+	# execute cmds and send files
 	#
-	sended = data_sock.send("SHARK"+file_name)
-	if sended != (len(file_name) + 5):
-		print "send file name failed"
-		exit()
-	time.sleep(2)
+	for cmdstring in cmdstrings:
+		cmds = cmdstring + idstring
+		print cmds
+		ret = os.system(cmds)
+		if ret != 0:
+			print cmds+"  error"
+			data_sock.close()
+			listen_sock2.close()
+			exit()
+		
+		file_name = cmds[cmds.find("/tmp"):]
+		print file_name
 	
-	# loops to send file data
-	#
-	open_file = open(file_name)
+		# send magic and filename
+		#
+		sended = data_sock.send("SHARK"+file_name)
+		if sended != (len(file_name) + 5):
+			print "send file name failed"
+			data_sock.close()
+			listen_sock2.close()
+			exit()
+		time.sleep(2)
+		
+		# loops to send file data
+		#
+		open_file = open(file_name)
+		while True:
+			data = open_file.read(1024)
+			if not data:
+				break
+			while len(data) > 0:
+				sended = data_sock.send(data)
+				data = data[sended:]
+		
+		time.sleep(2)
+		data_sock.send('EOF')
+		open_file.close()
+		
+	data_sock.close()
+	listen_sock2.close()
+
+
+if __name__ == '__main__':
+	
 	while True:
-		data = open_file.read(1024)
-		if not data:
-			break
-		while len(data) > 0:
-			sended = data_sock.send(data)
-			data = data[sended:]
-	
-	time.sleep(2)
-	data_sock.send('EOF')
-	open_file.close()
-	
-data_sock.close()
+		idstr = getidstr();
+		if len(idstr) <= 0:
+			print 'get idstr failed'
+			exit()
+		
+		sendfiles(idstr)
+
+
--- sharkserver.py
+++ sharkserver.py
@@ -10,65 +10,94 @@ import time
 # get requestid string from user to uniq this request
 # for result file storage
 # interactive or read config file
+#
+def getidstr():
+	idstring = raw_input("input 4 chars request idstring:")
+	while len(idstring) != 4:
+		idstring = raw_input("input 4 chars request idstring:")
+	return idstring
 
-idstring = raw_input("input 4 charactors request idstring:")
-while len(idstring) != 4:
-	idstring = raw_input("input 4 charactors request idstring:")
-	
-config_file = open("shark.conf")
-for conf_item in config_file:
-	print conf_item,
-	find_clientip = conf_item.find("clientip")
-	if find_clientip != -1:
-		client_ip_list = conf_item.split()
+# read conf to get clientip list
+#
+def readconf():
+	config_file = open("shark.conf")
+	for conf_item in config_file:
+		print conf_item,
+		find_clientip = conf_item.find("clientip")
+		if find_clientip != -1:
+			client_ip_list = conf_item.split()
+			return client_ip_list
 
 
 # cmdsocket to shake hands with clients
 # predef string as passwd
-# passwd andrequestid string as magic words
+# passwd and requestid string as magic words
+#
+def sendmagic(clientip, idstr):
+	cmdsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	cmdsock.connect((clientip,7778)) # TOD
+	cmdsock.send("qwe123+"+idstr)
+	cmdsock.close()
 
-cmdsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cmdsock.connect(('127.0.0.1',7778)) # TOD
-cmdsock.send("qwe123+"+idstring)
-cmdsock.close()
 
-time.sleep(2)
 
 # use data socket to recv result files
-
-datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-datasock.connect((clientip,7777))
-
-# if connected, file files to be recved each connection
 #
-file_counter = 5
-while file_counter > 0:
-	file_counter = file_counter - 1
-	raw_file_name = ""
-
-	# recv file name with magic "SHARK"
+def recvfiles(clientip):
+	datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	datasock.connect((clientip,7777))
+	
+	# if connected, file files to be recved each connection
 	#
-	raw_file_name = datasock.recv(48)
-	print raw_file_name
-	if raw_file_name.find("SHARK") != -1:
-		file_name = raw_file_name[raw_file_name.find("SHARK")+5:]
-		print "newfs " + file_name
-	else:
-		print "recv filename failed"
-		datasock.close()
+	file_counter = 5
+	while file_counter > 0:
+		file_counter = file_counter - 1
+		raw_file_name = ""
+	
+		# recv file name with magic "SHARK"
+		#
+		raw_file_name = datasock.recv(48)
+		print raw_file_name
+		if raw_file_name.find("SHARK") != -1:
+			file_name = raw_file_name[raw_file_name.find("SHARK")+5:]
+			print "newfs " + file_name
+		else:
+			print "recv filename failed"
+			datasock.close()
+			exit()
+		
+		# create new file ,recv data with results
+		# write file
+		#
+		openfile = open("/tmp"+file_name, 'w')
+		while True:
+			data = datasock.recv(1024)
+			if data == 'EOF':
+				break
+			openfile.write(data)
+		
+		openfile.close()
+	
+	datasock.close()
+	print client_ip + ' done'
+
+
+if __name__ == "__main__":
+
+	idstr = getidstr()
+
+	client_ip_list = readconf()
+	if client_ip_list == None:
+		print 'read conf failed'
 		exit()
-	
-	# create new file ,recv data with results
-	# write file
-	#
-	openfile = open("/tmp"+file_name, 'w')
-	while True:
-		data = datasock.recv(1024)
-		if data == 'EOF':
-			break
-		openfile.write(data)
-	
-	openfile.close()
 
-datasock.close()
-print 'done'
+	client_ip_list.reverse()
+	client_ip_list.pop()
+	print client_ip_list
+
+	while client_ip_list.__len__():
+		client_ip = client_ip_list.pop() 
+		sendmagic(client_ip, idstr)
+		time.sleep(2)
+		recvfiles(client_ip)
+
