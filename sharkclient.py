b66c1ee2f4a57cc09eb31c224aaf56a99af23397
jencce zhou <jencce2002@gmail.com>
2013-02-26 14:30:53 +0800

    add more comments

--- sharkclient.py
+++ sharkclient.py
@@ -1,18 +1,33 @@
 #! /usr/bin/env python
+# SHARK agent 
+#
+# daemon on target machine, after requestede, execute cmds to
+# collect perf data and write to datafile, sending files back 
+# to server
+#
 import socket
 import time
 import os
 
+# cmd strings needed to execute on client machines
+#
 cmdstrings = "sar -n DEV 1 10 > /tmp/sarnDEV-", \
 			 "iostat 1 10 > /tmp/iostat-",	\
 			 "sar -B 1 10 > /tmp/sarB-",	\
 			 "sar -P ALL 1 10 > /tmp/sarPALL-",	\
 			 "vmstat 1 10 > /tmp/vmstat-"
 
+# listen socket to accept cmd connection where get passwd 
+# and requestid.  if connection accepted, end blocking
+#
 listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_sock.bind(('',7778))
-listen_sock.listen(5)
+listen_sock.listen(25)
 cmd_sock,caddr = listen_sock.accept()
+
+# handshake to verify passwd
+# cmd socket to get cmd string
+#
 while True:
 	cmdstr = cmd_sock.recv(12)
 	if cmdstr[0:6] == 'qwe123':
@@ -24,11 +39,16 @@ idstring = cmdstr[cmdstr.find("+")+1:]
 
 cmd_sock.close()
 
-listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_sock.bind(('',7777))
-listen_sock.listen(5)
-data_sock,daddr = listen_sock.accept()
 
+# listen socket 2 to accept data connection
+#
+listen_sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_sock2.bind(('',7777))
+listen_sock2.listen(25)
+data_sock,daddr = listen_sock2.accept()
+
+# execute cmds 
+#
 for cmdstring in cmdstrings:
 	cmds = cmdstring + idstring
 	print cmds
@@ -41,12 +61,16 @@ for cmdstring in cmdstrings:
 	file_name = cmds[cmds.find("/tmp"):]
 	print file_name
 
+	# send magic and filename
+	#
 	sended = data_sock.send("SHARK"+file_name)
 	if sended != (len(file_name) + 5):
 		print "send file name failed"
 		exit()
 	time.sleep(2)
 	
+	# loops to send file data
+	#
 	open_file = open(file_name)
 	while True:
 		data = open_file.read(1024)
--- sharkserver.py
+++ sharkserver.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python
+# SHARK server 
+#
+# execute on any machine, request to SHARK agents to get
+# performance data files 
+#
 import socket
 import time
 
+# get requestid string from user to uniq this request
+# for result file storage
+# interactive or read config file
 
 idstring = raw_input("input 4 charactors request idstring:")
 while len(idstring) != 4:
@@ -15,20 +23,31 @@ for conf_item in config_file:
 		client_ip_list = conf_item.split()
 
 
+# cmdsocket to shake hands with clients
+# predef string as passwd
+# passwd andrequestid string as magic words
+
 cmdsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cmdsock.connect(('127.0.0.1',7778))
+cmdsock.connect(('127.0.0.1',7778)) # TOD
 cmdsock.send("qwe123+"+idstring)
 cmdsock.close()
 
 time.sleep(2)
 
+# use data socket to recv result files
+
 datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 datasock.connect((clientip,7777))
 
+# if connected, file files to be recved each connection
+#
 file_counter = 5
 while file_counter > 0:
 	file_counter = file_counter - 1
 	raw_file_name = ""
+
+	# recv file name with magic "SHARK"
+	#
 	raw_file_name = datasock.recv(48)
 	print raw_file_name
 	if raw_file_name.find("SHARK") != -1:
@@ -39,6 +58,9 @@ while file_counter > 0:
 		datasock.close()
 		exit()
 	
+	# create new file ,recv data with results
+	# write file
+	#
 	openfile = open("/tmp"+file_name, 'w')
 	while True:
 		data = datasock.recv(1024)
